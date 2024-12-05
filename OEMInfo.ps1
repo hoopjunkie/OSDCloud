c73a5f43eb6e9080af44e21be0b4ef8740147df2
hoopjunkie <67026859+hoopjunkie@users.noreply.github.com>
2024-12-05 07:38:53 -0700

    Update OEMInfo.ps1

--- OEMInfo.ps1
+++ OEMInfo.ps1
@@ -30,7 +30,7 @@ if(-not (Test-Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\OEMInformati
 
 
 #Download Logo from GitHub
-$LogoURL = "https://raw.githubusercontent.com/gwblok/garytown/master/OSD/CloudOSD/logo.bmp"
+$LogoURL = "https://raw.githubusercontent.com/hoopjunkie/OSDCloud/raw/refs/heads/main/logo.bmp"
 Invoke-WebRequest -UseBasicParsing -Uri $LogoURL -OutFile "$env:TEMP\logo.bmp"
 
 #Copy the file into place
